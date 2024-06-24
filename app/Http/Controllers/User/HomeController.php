84e5d9456741a51bceb99bbe2780d03b88af2e42
galih-rexy <galihrexyhakiki@gmail.com>
2024-06-25 00:52:08 +0700

    add testimonial

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -6,16 +6,19 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\News;
+use App\Models\Testimonial;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
         $news = News::latest()->take(3)->get();
+        $testimonial = Testimonial::all()->toArray();
 
         Inertia::setRootView('user');
         return Inertia::render('User/Index', [
-            'news' => $news
+            'news' => $news,
+            'testimonial' => $testimonial,
         ]);
     }
 }
