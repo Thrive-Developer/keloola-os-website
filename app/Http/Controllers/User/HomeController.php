15bb9020fdbb04e3113fe30df32eafa8c7845e28
devthrive <developer@thrive.co.id>
2024-08-01 19:21:34 +0700

    Fixing SEO Wording

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -7,6 +7,7 @@ use App\Models\Config;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\News;
+use App\Models\OsVersion;
 use App\Models\Testimonial;
 
 class HomeController extends Controller
@@ -17,13 +18,15 @@ class HomeController extends Controller
         $testimonial = Testimonial::all()->toArray();
         $button_download = Config::where('key', 'BUTTON_DOWNLOAD')->first();
         $button_installation_instruction = Config::where('key', 'BUTTON_INSTALLATION_INSTRUCTIONS')->first();
+        $latest_version = OsVersion::latest()->first();
 
         Inertia::setRootView('user');
         return Inertia::render('User/Index', [
             'news' => $news,
             'testimonial' => $testimonial,
             'button_download' => $button_download,
-            'button_installation_instruction' => $button_installation_instruction
+            'button_installation_instruction' => $button_installation_instruction,
+            'latest_version' => $latest_version
         ]);
     }
 }
